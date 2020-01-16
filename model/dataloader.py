422b456c9d33b34e3f435a0a5302f1d85be8550f
chhkang <chhkang123@gmail.com>
2020-01-16 11:26:50 +0900

    cifar10

--- model/dataloader.py
+++ model/dataloader.py
@@ -8,8 +8,8 @@ transform = transforms.Compose([
     transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5)),
 ])
 
-trainset = torchvision.datasets.CIFAR10(root='./data', split='train', download=True, transform=transform)
+trainset = torchvision.datasets.CIFAR10(root='./data', download=True, transform=transform)
 trainloader = torch.utils.data.DataLoader(trainset, batch_size=64, shuffle=True)
 
-testset = torchvision.datasets.CIFAR10(root='./data', split='test', download=True, transform=transform)
+testset = torchvision.datasets.CIFAR10(root='./data', download=True, transform=transform)
 testloader = torch.utils.data.DataLoader(testset, batch_size=64, shuffle=False)
