22524206a0cc66a64dbf1f583a09a8beb60fe912
chhkang <chhkang123@gmail.com>
2020-01-22 14:49:21 +0900

    changed files

---
+++ model.py
@@ -0,0 +1,57 @@
+import torch
+import torch.nn as nn
+
+
+class VGG16(nn.Module):
+    def __init__(self, num_classes=10):
+        super(VGG, self).__init__()
+        self.features = nn.Sequential(
+            # 3,64
+            nn.Conv2d(3, 64, kernel_size=3, stride=1, padding=1), nn.ReLU(inplace=True),
+            nn.Conv2d(64, 64, kernel_size=3, stride=1, padding=1), nn.ReLU(inplace=True),
+            nn.MaxPool2d(2, 2),
+            # 64,128
+            nn.Conv2d(64, 128, kernel_size=3, stride=1, padding=1), nn.ReLU(inplace=True),
+            nn.Conv2d(128, 128, kernel_size=3, stride=1, padding=1), nn.ReLU(inplace=True),
+            nn.MaxPool2d(2, 2),
+
+            # 128,256
+            nn.Conv2d(128, 256, kernel_size=3, stride=1, padding=1), nn.ReLU(inplace=True),
+            nn.Conv2d(256, 256, kernel_size=3, stride=1, padding=1), nn.ReLU(inplace=True),
+            nn.Conv2d(256, 256, kernel_size=3, stride=1, padding=1), nn.ReLU(inplace=True),
+            nn.MaxPool2d(2, 2),
+            # 256,512
+            nn.Conv2d(256, 512, kernel_size=3, stride=1, padding=1), nn.ReLU(inplace=True),
+            nn.Conv2d(512, 512, kernel_size=3, stride=1, padding=1), nn.ReLU(inplace=True),
+            nn.Conv2d(512, 512, kernel_size=3, stride=1, padding=1), nn.ReLU(inplace=True),
+            nn.MaxPool2d(2, 2),
+            # 512,512
+            nn.Conv2d(512, 512, kernel_size=3, stride=1, padding=1), nn.ReLU(inplace=True),
+            nn.Conv2d(512, 512, kernel_size=3, stride=1, padding=1), nn.ReLU(inplace=True),
+            nn.Conv2d(512, 512, kernel_size=3, stride=1, padding=1), nn.ReLU(inplace=True),
+            nn.MaxPool2d(2, 2),
+        )
+        # self.avg_pool = nn.AvgPool2d(4)
+        self.classifier = nn.Sequential(
+            nn.Dropout(),
+            nn.Linear(512, 4096),
+            nn.ReLU(inplace=True),
+            nn.Dropout(),
+            nn.Linear(4096, 1000),
+            nn.ReLU(inplace=True),
+            nn.Linear(1000, num_classes)
+        )
+
+    def forward(self, x):
+        x = self.features(x)
+        # x= self.avg_pool(x)
+        x = torch.flatten(x, 1)
+        x = self.classifier(x)
+        return x
+
+
+def vgg(data='cifar10'):
+    if data == 'cifar10':
+        return VGG16(num_classes=10)
+    elif data == 'cifar100':
+        return VGG16(num_classes=100)
